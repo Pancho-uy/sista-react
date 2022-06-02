86f37ff4e77f0a450d163d0b7ba9ee987fe6d824
Pancho-uy <pancho@cilbeti.com.uy>
2022-06-02 09:27:02 -0300

    menu bootstrap con icono de carrito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from "react";
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-  <NavBar></NavBar>
+  <NavBar/>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,45 @@
 // Menú principal
-import Navbar from "react-bootstrap/Navbar";
+import logo from "../logo_menu.png";
+import carrito from "../carrito.png";
 
 export default function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top fondoMio">
-    <section class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <section class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="index.html">
-              <img class= "img-fluid" src="/img/logo_menu.svg"></img> SISTA</a>
-          </li>
+    <>
+      <nav class="navbar navbar-expand-lg fixed-top fondoMio">
+        <section class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <section class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-            <a class="nav-link" href="secciones/nosotros.html">¿Quiénes Somos?</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="secciones/servicios.html">Servicios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="secciones/faq.html">F.A.Q.</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="secciones/contacto.html">Contacto</a>
-          </li>
-        </ul>
-      </section> 
-    </section> 
-    </nav> 
+                <a class="nav-link active" aria-current="page" href="index.html">
+                  <img src={logo} alt="logo"/></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Categorias</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ofertas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">F.A.Q.</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>              
+            </ul>
+          </section>
+          <section>
+            <a class="nav-link" href="../secciones/carrito.html">
+              <img class="float-end" src={carrito} alt="carrito" width="32" height="32"/>
+            </a>
+        </section>
+        </section>
+      </nav>
+    </>
   );
 }
