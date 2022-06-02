3455c6614be3a6a0293853dc615122abf5dfd263
Pancho-uy <pancho@cilbeti.com.uy>
2022-06-02 09:40:45 -0300

    texto blanco en menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import carrito from "../carrito.png";
 export default function NavBar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg fixed-top fondoMio">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top fondoMio">
         <section class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,7 +14,7 @@ export default function NavBar() {
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="index.html">
-                  <img src={logo} alt="logo"/></a>
+                  <img src={logo} alt="logo"/><strong>SISTA</strong></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Categorias</a>
