19621b53a6f90e86de4710666f6f84a066e291b4
Pancho-uy <pancho@cilbeti.com.uy>
2022-07-03 13:52:21 -0300

    cartview

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const Item = ({ item }) => {
     const miURL=`/detalle/${item.ID}`;
     return (
-        <div class="col-sm">
-            <div className="card">
+        <div class="col">
+            <div className="card h-100 ">
                 <div className="card-body">
                     <NavLink to={miURL}>
                         <img src={item.img} alt="imagen del articulo"></img>
