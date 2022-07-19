bc7c0f4d3a353fc33e4df445dfb08efca58a06bc
Pancho-uy <pancho@cilbeti.com.uy>
2022-07-19 11:29:14 -0300

    proyecto react

--- src/components/formUSR.jsx
+++ src/components/formUSR.jsx
@@ -27,9 +27,9 @@ function FormUSR({cart, totalCompra, creoOrdenDeCompra, clearCart}) {
       total: totalCompra(),
     };
     console.log("NOMBRE COMPRADOR: ",dataOrder.buyer.name);
-    dataOrder.buyer.name === "" || dataOrder.buyer.email === "" ? alert("Por favor, Nombre e Email son obligatorios") : creoOrdenDeCompra(dataOrder);
+    dataOrder.buyer.name === "" || dataOrder.buyer.email === "" ? alert("Por favor, Nombre e Email son obligatorios") :
     creoOrdenDeCompra(dataOrder).then(( orderDataCreated ) => {<div>${dataOrder}</div>
-    alert('¡Gracias por su compra!\n\nSu orden de compra ha sido creada con el número: ' + orderDataCreated.id+
+    alert('¡Gracias por su compra!\n\nSu orden de compra ha sido creada con el ID: ' + orderDataCreated.id+
     '\n\nLe enviaremos un email a: '+dataOrder.buyer.email+' a la brevedad para coordinar la entrega.');
       clearCart();});
   }
